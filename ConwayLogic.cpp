638c292e0bd9e06472eb94e2fda765f276374862
TravisEvans <travisjamesevans01@gmail.com>
2021-04-06 19:17:00 +1000

    works

--- ConwayLogic.cpp
+++ ConwayLogic.cpp
@@ -13,15 +13,17 @@ using namespace Conways;
 void resizeCalculationGrid(int displaySizeX, int displaySizeY, vector<vector<Cell>> &calculationGrid);
 void iterateThroughBothGrids(vector<vector<Cell>> displayGrid, vector<vector<Cell>> &calculationGrid);
 void calculateCell(int cellX, int cellY, vector<vector<Cell>> displayGrid, vector<vector<Cell>> &calculationGrid);
+void populateCalculationGrid(int cellX, int cellY, int (&cluster)[3][3], vector<vector<Cell>> &displayGrid, vector<vector<Cell>> &calculationGrid);
 void displayCalculationGrid(vector<vector<Cell>> calculationGrid);
+void replaceDisplay(vector<vector<Cell>> &displayGrid, vector<vector<Cell>> &calculationGrid);
 
-void gameStep(vector<vector<Cell>> displayGrid, vector<vector<Cell>> calculationGrid)
+void gameStep(vector<vector<Cell>> &displayGrid, vector<vector<Cell>> &calculationGrid)
 {
     resizeCalculationGrid(displayGrid[0].size(), displayGrid.size(), calculationGrid);  //  makes grids same size
 
     iterateThroughBothGrids(displayGrid, calculationGrid);
-    // calculateCell();
-    displayCalculationGrid(calculationGrid);
+    replaceDisplay(displayGrid, calculationGrid);
+    // displayCalculationGrid(calculationGrid); //  mainly for testing
 }
 
 void resizeCalculationGrid(int displaySizeX, int displaySizeY, vector<vector<Cell>> &calculationGrid)
@@ -38,73 +40,77 @@ void iterateThroughBothGrids(vector<vector<Cell>> displayGrid, vector<vector<Cel
     //  x and y are what has been checked
     for (long unsigned int y = 0; y < displayGrid.size(); y++)    {
         for (long unsigned int x = 0; x < displayGrid[0].size(); x++) {
-            calculateCell(x, y, displayGrid, calculationGrid);   //  ERROR
+            calculateCell(x, y, displayGrid, calculationGrid);
         }
     }
 }
 
 void calculateCell(int cellX, int cellY, vector<vector<Cell>> displayGrid, vector<vector<Cell>> &calculationGrid)
 {
-    //  make array of cell and neighbours where
-    /*
-        0,  0, 0
-        0,cell,0
-        0,  0, 0
-    */
-
-    //  holder for cell + neighbours
+    //  get real values from displayGrid into cluster[][] and send elsewhere to populate calculationGrid
     int cluster[3][3] = {   
         {0, 0, 0},  // 0,1,2
         {0, 0, 0},  // 3,4,5
         {0, 0, 0}}; // 6,7,8
-
-    //  get real values from displayGrid into cluster[][]
     for (int ymod = 0; ymod < 3; ymod++) {
-        for (int xmod = 0; xmod < 3; xmod++) {
-            // try {
-            //     cluster[1+(ymod-1)][1+(xmod-1)] = displayGrid[cellY+ymod][cellX+xmod].getState();   //  ERROR
-            // } catch (...)   {
-            //     cluster[1+ymod][1+xmod] = 0;
-            // }
-            // if (displayGrid[cellY + ymod][cellX + xmod].getState() == NULL) {
-            //     // cout << "hit if" << endl;
-            //     cluster[1+ymod][1+xmod] = 0;
-            // } else  {
-            //     // cout << "hit else" << endl;
-            //     cluster[1+(ymod-1)][1+(xmod-1)] = displayGrid[cellY+ymod][cellX+xmod].getState();   //  FIXME
-            // }
+        //  holder for cell + neighbours
 
-            if (cellY + (ymod-1) < 0 ||
-                (unsigned long int)(cellY + (ymod-1)) > (displayGrid.size()-1) ||   // fuck .size()
-                cellX + (xmod-1) < 0 ||
-                (unsigned long int)(cellX + (xmod-1)) > displayGrid[0].size())
+        for (int xmod = 0; xmod < 3; xmod++) {
+
+            if ((cellY + (ymod-1)) < 0 ||
+                (signed int)(cellY + (ymod-1)) > (signed int)(displayGrid.size()-1) ||   // fuck .size() and stupid seg faults
+                (cellX + (xmod-1)) < 0 ||
+                (signed int)(cellX + (xmod-1)) > (signed int)(displayGrid[0].size()-1))      //  why the fuck does this .size() work????? I even triple checked?!?!?!?
             {
-                cout << "hit if" << endl;
                 cluster[1+(ymod-1)][1+(xmod-1)] = 0;
+                // cout << cluster[1+(ymod-1)][1+(xmod-1)] << "|";
             }
             else
             {
+                // bool life = displayGrid[cellY+(ymod-1)][cellX+(xmod-1)].getState();
+                // cout << displayGrid[cellY+(ymod-1)][cellX+(xmod-1)].getState() << "\t" << life << endl;
+                // life == true ? cluster[1+(ymod-1)][1+(xmod-1)] = 1 : cluster[1+(ymod-1)][1+(xmod-1)] = 0;
                 cluster[1+(ymod-1)][1+(xmod-1)] = displayGrid[cellY+(ymod-1)][cellX+(xmod-1)].getState();
+                // cout << cluster[1+(ymod-1)][1+(xmod-1)] << "|";
+                // cout << life << endl;
             }
-
-        // cout << "hit after" << endl;
         }
-    }
 
-    //  display the above, for testing reasons
-    cout << endl;
-    cout << cluster[0][0];
-    cout << cluster[0][1];
-    cout << cluster[0][2];
-    cout << endl;
-    cout << cluster[1][0];
-    cout << cluster[1][1];
-    cout << cluster[1][2];
-    cout << endl;
-    cout << cluster[2][0];
-    cout << cluster[2][1];
-    cout << cluster[2][2];
-    cout << endl;
+    }
+    populateCalculationGrid(cellX, cellY, cluster, displayGrid, calculationGrid);    //  aforementioned 'elsewhere'
+    // cout << "cluster values for " << cellY << cellX << ":\n" << cluster[0][0] << "|" << cluster[0][1] << "|" << cluster[0][2] << "|\n" << cluster[1][0] << "|" << cluster[1][1] << "|" <<  cluster[1][2] << "|\n" << cluster[2][0] << "|" << cluster[2][1] << "|" << cluster[2][2] << "|" << endl;
+    // cout << "total neighbours:\t " << (cluster[0][0] + cluster[0][1] + cluster[0][2] +
+    //                                       cluster[1][0] + /*    cell + */ cluster[1][2] + cluster[2][0] + cluster[2][1] + cluster[2][2]) << endl;
+    //  UP TO HERE IS GOOD
+}
+
+void populateCalculationGrid(int cellX, int cellY, int (&cluster)[3][3], vector<vector<Cell>> &displayGrid, vector<vector<Cell>> &calculationGrid)
+{
+    //  this is where the rules come in, calculated from the cluster
+    //  remember, ignore cluster[1][1]
+    int neighbours = cluster[0][0] + cluster[0][1] + cluster[0][2] + // this is the issue ERROR
+                         cluster[1][0] + /*    cell + */ cluster[1][2] + 
+                         cluster[2][0] + cluster[2][1] + cluster[2][2];
+
+    // cout << "cluster values for " << cellY << cellX << ":\n" << cluster[0][0] << "|" << cluster[0][1] << "|" << cluster[0][2] << "|\n" << cluster[1][0] << "|" << cluster[1][1] << "|" <<  cluster[1][2] << "|\n" << cluster[2][0] << "|" << cluster[2][1] << "|" << cluster[2][2] << "|" << endl;
+    // cout << "total neighbours:\t " << (cluster[0][0] + cluster[0][1] + cluster[0][2] +
+    //                                       cluster[1][0] + /*    cell + */ cluster[1][2] + cluster[2][0] + cluster[2][1] + cluster[2][2]) << endl;
+
+    //  rule 1. if <2 neighbours, then die
+    //  rule 2. if 2 or 3 neighbours, then stay the same
+    //  rule 3. if 3 neighbours, then become alive (from dead)
+    //  rule 4. if >3 neighbours, then die
+    
+
+    if (neighbours < 2) {
+        calculationGrid[cellY][cellX].setDead();
+    } else if (neighbours > 3)  {
+        calculationGrid[cellY][cellX].setDead();
+    } else if (neighbours == 3)  {
+        calculationGrid[cellY][cellX].setAlive();
+    } else if (neighbours == 2 && (displayGrid[cellY][cellX].getState() == true))    {
+        calculationGrid[cellY][cellX].setAlive();
+    }
 }
 
 void displayCalculationGrid(vector<vector<Cell>> calculationGrid)
@@ -112,8 +118,21 @@ void displayCalculationGrid(vector<vector<Cell>> calculationGrid)
     for (vector<Cell> cellVec : calculationGrid)    {
         cout << "\t\t";
         for (Cell c : cellVec)  {
-            cout << c.getState();
+            if (c.getState() == true)
+                cout << "□"; //□
+            else
+                cout << "x";
         }
         cout << endl;
+    }
+}
+
+void replaceDisplay(vector<vector<Cell>> &displayGrid, vector<vector<Cell>> &calculationGrid)   {
+    for (int i = 0; i < (int)displayGrid.size(); i++)   {
+        for (int j =0; j < (int)displayGrid[0].size(); j++)  {
+
+            displayGrid[i][j] = calculationGrid[i][j];
+                   
+        }
     }
 }
--- ConwaysGameOfLife.cpp
+++ ConwaysGameOfLife.cpp
@@ -3,6 +3,9 @@
 #include <vector>
 #include <stdlib.h> // rand
 #include <time.h>   //  time
+#include <chrono>
+#include <thread>
+
 using namespace std;
 
 #ifndef Conways
@@ -10,12 +13,21 @@ using namespace std;
 using namespace Conways;
 #endif
 
+// #ifdef _WIN32
+// #include <Windows.h>    //  Windows
+// #else
+// #include <unistd.h> //  linux
+// #endif
+
 // declarations
-void run(vector<vector<Cell>> displayGrid, vector<vector<Cell>> calculationGrid);
+void run(vector<vector<Cell>> &displayGrid, vector<vector<Cell>> &calculationGrid);
 bool checkForLife(vector<vector<Cell>> displayGrid);
 void populateCellGrid(vector<vector<Cell>> &displayGrid);
 void displayCellGrid(vector<vector<Cell>> displayGrid);
-void gameStep(vector<vector<Cell>> displayGrid, vector<vector<Cell>> calculationGrid);
+void gameStep(vector<vector<Cell>> &displayGrid, vector<vector<Cell>> &calculationGrid);
+
+//  constants
+const int MS_PER_FRAME = 0; // milliseconds
 
 /**
  * main function
@@ -26,12 +38,15 @@ int main()
     vector<vector<Cell>> displayGrid;
     vector<vector<Cell>> calculationGrid;
 
-    run(displayGrid, calculationGrid);  //  single instance of grids
+    while (true)    {
+        run(displayGrid, calculationGrid);  //  single instance of grids
+        this_thread::sleep_for(chrono::milliseconds(MS_PER_FRAME));
+    }
 
     return 0;
 }
 
-void run(vector<vector<Cell>> displayGrid, vector<vector<Cell>> calculationGrid)
+void run(vector<vector<Cell>> &displayGrid, vector<vector<Cell>> &calculationGrid)
 {
     if (!checkForLife(displayGrid))    {   //  so, if there is no life, do...
         populateCellGrid(displayGrid);   //  populate dead grid
@@ -42,7 +57,7 @@ void run(vector<vector<Cell>> displayGrid, vector<vector<Cell>> calculationGrid)
 
     displayCellGrid(displayGrid); //  see how life is going
     cout << endl;
-    gameStep(displayGrid, calculationGrid);    //  see the calculation grid
+    gameStep(displayGrid, calculationGrid);    //  step
 
     cout << "\n\n\n" << endl;
 }
@@ -60,15 +75,14 @@ bool checkForLife(vector<vector<Cell>> displayGrid)    {   //  check if life has
 }
 
 /**
- * Currently populates a referenced displayGrid with arbitrary numbers.
- * TODO: add random population / generation
+ * Currently randomly populates a referenced displayGrid with "life" cells
  */
 void populateCellGrid(vector<vector<Cell>> &displayGrid)
 {
-    for (int i = 0; i < 10; i++)    { //  10 is a magic number, basically the min y size
+    for (int i = 0; i < 50; i++)    { //  10 is a magic number, basically the min y size
     vector<Cell> cellVec;
     displayGrid.push_back(cellVec);
-        for (int j = 0; j < 20; j++){ //  20 is a magic number, basically the min x size
+        for (int j = 0; j < 100; j++){ //  20 is a magic number, basically the min x size
             Cell c;
             //  get random number for use in generation
             srand(time(NULL)*rand());  //  seed1
@@ -85,14 +99,25 @@ void populateCellGrid(vector<vector<Cell>> &displayGrid)
 
 void displayCellGrid(vector<vector<Cell>> displayGrid)
 {
+    cout << "\t\t ";  // border top
+    for (int i = 0; i < (int)displayGrid[0].size(); i++)
+        cout << "-";
+    cout << endl;
+    
     for (vector<Cell> cellVec : displayGrid)
     {
-        cout << "\t\t";
+        cout << "\t\t|";    //  border left
         for (Cell cell : cellVec)
-        {
-            cout << cell.getState();
-            //□ 
+        {   
+            if (cell.getState() == true)
+                cout << "o";
+            if (cell.getState() == false)
+                cout << " ";
         }
+        cout << "|";    //  border right
         cout << endl;
     }
+    cout << "\t\t "; // border bottom
+    for (int i = 0; i < (int)displayGrid[0].size(); i++)
+        cout << "-";
 }
